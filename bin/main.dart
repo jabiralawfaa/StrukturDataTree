9ce07348b9e1f8f503da47fcd4210b68b35c714e
jabiralawfaa <162452844+jabiralawfaa@users.noreply.github.com>
2025-05-25 13:47:54 +0700

    Chalenge v1

---
+++ bin/main.dart
@@ -0,0 +1,41 @@
+import 'tree_project.dart';
+
+void main() {
+  BinaryTree tree = BinaryTree();
+
+  tree.insertNodeLevelOrder(50);
+  tree.insertNodeLevelOrder(true);
+  tree.insertNodeLevelOrder(48);
+  tree.insertNodeLevelOrder('A');
+  tree.insertNodeLevelOrder(30);
+  tree.insertNodeLevelOrder(20.5);
+  tree.insertNodeLevelOrder(10);
+  tree.insertNodeLevelOrder(1);
+
+  print("inorder iterative: ");
+  tree.inorderIterative(tree.root);
+
+  print("\npreorder iterative: ");
+  tree.preorderIterative(tree.root);
+
+  print("\npostorder iterative: ");
+  tree.postorderIterative(tree.root);
+
+  // print("Preorder traversal:");
+  // tree.preorder(tree.root);
+
+  // print("\nInorder traversal:");
+  // tree.inorder(tree.root);
+
+  // print("\nPostorder traversal:");
+  // tree.postorder(tree.root);
+
+  int n = 3;
+  print("\nlevel order traversal of $n in binary tree is (recursion):");
+  tree.printLevelOrder(tree.root,n);
+
+  print("\nlevel order traversal of $n is binary tree is (non recursion) :");
+  tree.printLevelOrderQueue(tree.root, n);
+
+  print("\nHeight of tree is : ${tree.maxDepth(tree.root)}");
+}
---
+++ test/tree_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:tree_project/tree_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
